5cea0dcee88c6fae1abb8acc7115acd65a1db9a4
hayamishun <shun.883@goole.com>
2019-11-03 11:02:53 +0900

    5の倍数のFizzBuzzに対応

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,12 @@ def fizzbuzz_convert(number):
     if number % 3 == 0:
         return 'Fizz'
 
+    if number % 5 == 0:
+        return 'Buzz'
+
 
 assert fizzbuzz_convert(3) == 'Fizz'
 assert fizzbuzz_convert(6) == 'Fizz'
+
+assert fizzbuzz_convert(5) == 'Buzz'
+assert fizzbuzz_convert(10) == 'Buzz'
